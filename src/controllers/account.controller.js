531da8f73aab5034b4d210adfd9353b518603729
yashkatara-coder <yashkatara436@gmail.com>
2026-09-23 19:29:59 +0530

    Add account balance endpoint

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -24,8 +24,31 @@ async function getUserAccountsController(req, res) {
     })
 }
 
+async function getAccountBalanceController(req, res) {
+    const { accountId } = req.params;
+
+    const account = await accountModel.findOne({
+        _id: accountId,
+        user: req.user._id
+    })
+
+    if (!account) {
+        return res.status(404).json({
+            message: "Account not found"
+        })
+    }
+
+    const balance = await account.getBalance();
+
+    res.status(200).json({
+        accountId: account._id,
+        balance: balance
+    })
+}
+
 
 module.exports = {
     createAccountController,
-    getUserAccountsController
+    getUserAccountsController,
+    getAccountBalanceController
 }
--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -14,4 +14,10 @@ router.post("/",authMiddleware.authMiddleware,accountController.createAccountCon
  */
 router.get("/", authMiddleware.authMiddleware, accountController.getUserAccountsController)
 
+/**
+ * - GET /api/accounts/balance/:accountId
+ */
+router.get("/balance/:accountId", authMiddleware.authMiddleware, accountController.getAccountBalanceController)
+
+
 module.exports = router;
